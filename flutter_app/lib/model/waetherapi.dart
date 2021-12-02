1a141b2840609eaf47712af4244edba321abb343
Florian Büchi <54060167+Flopi04LP@users.noreply.github.com>
2021-12-02 08:22:13 +0100

    Update waetherapi.dart

--- flutter_app/lib/model/waetherapi.dart
+++ flutter_app/lib/model/waetherapi.dart
@@ -3,6 +3,6 @@ import 'package:flutter_app/screens/weatherapp.dart';
 import 'package:flutter_app/main.dart';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<http.Response> fetchWeather() {
+  return http.get(Uri.parse(''));
 }
