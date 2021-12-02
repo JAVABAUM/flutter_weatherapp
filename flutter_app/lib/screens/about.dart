05e604e19b7aca083007cf9119d88c2876ed4c07
FlyLikeAle <a.baumgartner.inf19@stud.bbbaden.ch>
2021-12-02 08:51:18 +0100

    Update about.dart

--- flutter_app/lib/screens/about.dart
+++ flutter_app/lib/screens/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/weatherapp.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class about extends StatelessWidget {
   const about({Key? key}) : super(key: key);
@@ -10,21 +11,48 @@ class about extends StatelessWidget {
       appBar: AppBar(
         title: const Text("About Page"),
       ),
-      body: Container(
-        child: Stack(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => WeatherApp()),
-                );
-              },
-              child: Text("Go Back"),
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => WeatherApp()),
+              );
+            },
+            child: const Text("Go Back"),
+          ),
+          Row(
+            children: <Widget>[
+              const Text(
+                'This is me',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(width: 50),
+              FlatButton(
+                onPressed: () {
+                  _launchURL();
+                },
+                minWidth: 20,
+                child: Image.asset(
+                  'assets/images/instagram.png',
+                  height: 20,
+                  width: 20,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
+  }
+}
+
+_launchURL() async {
+  const url = 'https://instagram.com/d9geri';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
